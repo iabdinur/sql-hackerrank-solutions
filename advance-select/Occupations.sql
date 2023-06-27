5ca8f2c448ec745aa63d108b6345d4e0ea877f99
iabdinur <iabdinur1@gmail.com>
2023-06-28 00:29:08 +0100

    sql-advance-select update

--- advance-select/Occupations.sql
+++ advance-select/Occupations.sql
@@ -17,3 +17,13 @@ The OCCUPATIONS table is described as follows:
 Occupation will only contain one of the following values: Doctor, Professor, Singer or Actor.
 
 */
+
+SELECT 
+    Min(IF(Occupation="Doctor", Name, NULL)) AS Doctor,
+    Min(IF(Occupation="Professor", Name, NULL)) AS Professor,
+    Min(IF(Occupation="Singer", Name, NULL)) AS Singer,
+    Min(IF(Occupation="Actor", Name, NULL)) AS Actor
+FROM         
+    (Select Name, Occupation, Row_number() OVER (PARTITION BY Occupation ORDER BY name) AS row_num From OCCUPATIONS) 
+
+AS ord GROUP BY row_num
