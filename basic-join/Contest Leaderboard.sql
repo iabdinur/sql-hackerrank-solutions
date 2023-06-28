810bef809ec29da433a99ff787692e5fb28c31d7
iabdinur <iabdinur1@gmail.com>
2023-06-28 02:19:01 +0100

    sql-basic-join updated

--- basic-join/Contest Leaderboard.sql
+++ basic-join/Contest Leaderboard.sql
@@ -31,17 +31,17 @@ challenge_id is the id of the challenge for which the submission belongs to, and
 
 */
 
-SELECT 
+select 
 	h.hacker_id, 
     h.name, 
-    SUM(MAX_SCORE.t1) AS total_score
-FROM Hackers h inner join 
+    SUM(MAX_SCORE.t1) as total_score
+from Hackers h inner join 
 (
-    SELECT MAX(s.score) AS t1, s.hacker_id  
-    FROM Submissions s
-    GROUP BY s.challenge_id, s.hacker_id
-    HAVING t1 > 0
+    select MAX(s.score) as t1, s.hacker_id  
+    from Submissions s
+    group by s.challenge_id, s.hacker_id
+    having t1 > 0
 ) AS MAX_SCORE
 ON h.hacker_id = MAX_SCORE.hacker_id
-GROUP BY h.hacker_id, h.name
-ORDER BY total_score DESC, hacker_id ASC
+group by h.hacker_id, h.name
+order by total_score desc, hacker_id asc
